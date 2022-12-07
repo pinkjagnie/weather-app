eb55e950458b37ff05d11edd1d23053bcfa97302
pinkjagnie <olga.krzysztofik@gmail.com>
2022-12-07 16:56:01 +0100

    Added styles to HomePage, SearchBar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import SearchBar from "./SearchBar";
 
+import styles from "./homePage.module.css";
+
 const HomePage = () => {
   return(
-    <div>
-      <div>
+    <div className={styles.homePageSection}>
+      <div className={styles.homePageText}>
         <h1>Weather forecast app</h1>
         <h2>Don't be surprised! Check the weather before you leave the house</h2>
       </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,14 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
+import styles from "./searchBar.module.css";
+
 const SearchBar = () => {
   return(
-    <div>
-      <form>
-        <input type="text" placeholder="Search for location" required autoFocus/>
-        <button><FontAwesomeIcon icon={faSearch} /></button>
+    <div className={styles.searchBarSection}>
+      <form className={styles.searchBarForm}>
+        <input type="text" placeholder="Search for location" required autoFocus className={styles.searchBarInput}/>
+        <button className={styles.searchBarButton}><FontAwesomeIcon icon={faSearch} className={styles.searchBarIcon} /></button>
       </form>
     </div>
   )
