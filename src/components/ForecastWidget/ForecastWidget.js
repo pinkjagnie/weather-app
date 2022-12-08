7f9854124eae6ac30702fc3a1c4f399f1d80df1e
pinkjagnie <olga.krzysztofik@gmail.com>
2022-12-08 11:32:16 +0100

    Clean stuff

--- src/components/ForecastWidget/ForecastWidget.js
+++ src/components/ForecastWidget/ForecastWidget.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import CurrentDay from "./CurrentDay";
 import CurrentDayDescription from "./CurrentDayDescription";
-import UpcomingDays from "./UpcomingDays";
 
 import styles from "./forecastWidget.module.css";
 
@@ -12,7 +11,6 @@ const ForecastWidget = ({forecast}) => {
       <CurrentDay forecast={forecast}/>
       <div className={styles.forecastWidgetMore}>
         <CurrentDayDescription forecast={forecast}/>
-        <UpcomingDays forecast={forecast}/>
       </div>
     </div>
   )
